4c23bc7fba30717e33b4a486042dc3dd6d088470
rehdias <renata_dias96@live.com>
2021-11-26 19:33:01 -0300

    Exercicios/4.1

---
+++ 1 - Fundamentos/Bloco04 - Introdução à JavaScript e lógica de programação/4.1 - JavaScript - Primeiros passos/4.1.js
@@ -0,0 +1,101 @@
+//// Exercício 1
+
+// const a = 2;
+// const b = 6;
+// let adicao = a + b;
+// let subtracao = a - b;
+// let multiplicacao = a * b;
+// let divisao = a / b;
+// let modulo = a % b;
+// console.log(adicao);
+// console.log(subtracao);
+// console.log(multiplicacao);
+// console.log(divisao);
+// console.log(modulo);
+
+//// Exercício 2
+
+// const num1 = 50;
+// const num2 = 20;
+// if (num1>num2) {
+//   console.log("O primeiro número é maior");
+// }
+// else if (num1<num2) {
+//   console.log("O segundo número é maior");
+// }
+// else if (num1===num2){
+//   console.log("Os números são igauis"); 
+// }
+
+
+//// Exercício 3
+
+// const numero1 = 60;
+// const numero2 = 70;
+// const numero3 = 30;
+// if ((numero1>numero2) && (numero1>numero3)) {
+//   console.log("O primeiro número é maior");
+// }
+// else if ((numero2>numero1) && (numero2>numero3)) {
+//   console.log("O segundo número é maior");
+// }
+// else {
+//   console.log("O terceiro número é maior");
+// }
+
+
+//// Exercício 4
+
+// const number = -15;
+// if (number > 0) {
+//   console.log("positive");
+// }
+// else if (number < 0) {
+//   console.log("negative");
+// }
+// else {
+//   console.log("zero");
+// }
+
+
+//// Exercício 5
+
+// const ang1 = 45;
+// const ang2 = 45;
+// const ang3 = 90;
+// if ((ang1+ang2) + ang3 ===180) {
+//   console.log("true");
+// } 
+// else {
+//   console.log("false");
+// }
+
+
+//// Exercício 6
+
+// let xadrez = "Cavalo";
+// switch (xadrez.toLowerCase()) {
+//   case "rei":
+//     console.log("Movimenta-se uma casa em qualquer direção");
+//   break;
+
+//   case "rainha":
+//     console.log("Movimenta-se para frente ou para trás, para direita ou para a esquerda, ou na diagonal, quantas casas quiser");
+//   break;
+
+//   case "cavalo":
+//     console.log("Movimenta-se  2 casas para frente ou para trás e em seguida 1 casa para a direita ou para a esquerda, ou 2 casas para a direita ou para a esquerda e em seguida 1 casa para frente ou para trás");
+//   break;
+
+//   case "torre":
+//     console.log("Movimenta-se para frente ou para trás, para a direita ou para a esquerda, quantas casas quiser");
+//   break;
+
+//   case "bispo":
+//     console.log("Movimenta-se na diagonal mantendo-se sempre nas casas de mesma cor que se encontrava no início do jogo, podendo ir para frente e para trás, quantas casas quiser");
+//   break;
+
+//   case "peao":
+//     console.log("Movimenta-se uma casa para frente");
+//   break;
+// }

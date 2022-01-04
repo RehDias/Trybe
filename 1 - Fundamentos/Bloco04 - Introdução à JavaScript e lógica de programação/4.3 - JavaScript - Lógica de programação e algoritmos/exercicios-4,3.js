0620b7cf890442fee852b82e93e22d8d72bc7864
Renata Dias <renata_dias96@live.com>
2022-01-04 15:11:43 -0300

    Exemplos dia 6.1

---
+++ 1 - Fundamentos/Bloco04 - Introdução à JavaScript e lógica de programação/4.3 - JavaScript - Lógica de programação e algoritmos/exercicios-4,3.js
@@ -0,0 +1,23 @@
+let asterisco = "*";
+let linha = [];
+
+//// Exercício 1
+
+for (let i = 1; i <= 5; i +=1) {
+  linha.push(asterisco * i);     
+}
+
+for (let index = 0; index < linha.length; index +=1) {
+  console.log(linha);
+}
+
+
+//// Exercício 2
+
+// for (let i = 1; i <= 5; i +=1) {
+//   linha.push(asterisco * i);   
+//       for (let index = 0; index < linha.length; index+=1) {
+//       console.log(linha);
+//     }
+// }
+
---
+++ 1 - Fundamentos/Bloco05 - JavaScript_ DOM, Eventos e Web Storage/5.1 - JavaScript - DOM e Seletores/script.js
@@ -0,0 +1,6 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+document.getElementById("page-title").innerText = "Os vingadores";
+document.getElementById("second-paragraph").innerText = "Existe uma franquia de filmes dos vingadores, que vale muito a pena assistir.";
+document.getElementById("subtitle").innerText = "O melhor filme de super-heróis";
+document.getElementsByClassName("paragrafo")[0].style.color = green;

aea6fdae99bd21c04988cec9949ec74489ade6e0
Renata Dias <renata_dias96@live.com>
2022-01-04 21:44:48 -0300

    Adiciona dados de emprego

--- 1 - Fundamentos/Bloco06 - HTML e CSS: Forms, Flexbox e Responsivo/6.1 - HTML & CSS - Forms/script.js
+++ 1 - Fundamentos/Bloco06 - HTML e CSS: Forms, Flexbox e Responsivo/6.1 - HTML & CSS - Forms/script.js
@@ -1,6 +1,19 @@
 let state = document.getElementById('input-state');
 let estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe',
 'Tocantins'];
+let date = document.getElementById('input-date');
+let form = document.getElementsByClassName('formulario');
+let botao = document.getElementsByClassName('botao');
+
+function interromper(){
+  form.preventDefault();
+}
+
+function verifyDate(){
+  if(date !== dd/mm/aaaa){
+    alert('erro contextualizada');
+  }
+}
 function gerateState(){
   for(let i=0; i<estados.length; i+=1){
     let estado = document.createElement('option');
